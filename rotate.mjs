e776b6e94c1acd126c1aad556606ef3db86fb85d
mylifeback <mauricewong@mac.com>
2025-05-18 19:51:36 +0800

    update

--- rotate.mjs
+++ rotate.mjs
@@ -1,7 +1,9 @@
 export default class RotateScript {
+  speed = 90;
   update(dt) {
     // Rotate the entity 90 degrees per second around the world-space Y axis
     this.entity.rotate(0, dt * 90, 0);
+    console.log(2);
   }
 }
 
